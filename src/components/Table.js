19899d9c7922799fcca973620d3a68ca73cda46a
roman-yaremiy <ryaremiy@conceptsoft.io>
2023-06-26 14:20:30 +0300

    styled-compnents added

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { getStoreData } from '../dbService';
 import { List } from "react-virtualized";
+import styled from 'styled-components';
+
+const Row = styled.div`
+  display: flex;
+`;
 
 const Table = ({ rows }) => {
   if (!rows.length) return null;
@@ -8,76 +13,78 @@ const Table = ({ rows }) => {
   console.log('rows > ', rows);
 
   const renderRow = ({ index, key, style }) => (
-    <div className={rows[index].rowId}>
-      <div key={rows[index].rowId} style={{ display: 'flex' }}>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-        <div>{rows[index].rowId}</div>
-        <div>{rows[index].id}</div>
-      </div>
-    </div>
+    <Row
+      key={rows[index].rowId}
+      className={rows[index].rowId}
+    >
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+      <div>{rows[index].rowId}</div>
+      <div>{rows[index].id}</div>
+    </Row>
   );
 
   return (
     <List
       width={1200}
       height={700}
+      // autoHeight={true}
       rowRenderer={renderRow}
       rowCount={rows.length}
       rowHeight={120}
