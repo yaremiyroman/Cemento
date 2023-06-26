2023581922411c94f18d3eb5e0e84d660490f989
roman-yaremiy <ryaremiy@conceptsoft.io>
2023-06-26 14:02:30 +0300

    react-virtualized added

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,48 +1,87 @@
 import React, { useState, useEffect } from "react";
 import { getStoreData } from '../dbService';
+import { List } from "react-virtualized";
 
 const Table = ({ rows }) => {
   if (!rows.length) return null;
 
   console.log('rows > ', rows);
 
-  return (
-    <div>
-      {rows.map(({ id, rowId }) => (
-        <div key={rowId}>
-          <div>
-            {rowId}
-          </div>
-          <div>
-            {id}
-          </div>
-          <div>
-            {rowId}
-          </div>
-          <div>
-            {id}
-          </div>
-          <div>
-            {rowId}
-          </div>
-          <div>
-            {id}
-          </div>
-          <div>
-            {rowId}
-          </div>
-          <div>
-            {id}
-          </div>
-          <div>
-            {rowId}
-          </div>
-          <div>
-            {id}
-          </div>
-        </div>
-      ))}
+  const renderRow = ({ index, key, style }) => (
+    <div className={rows[index].rowId}>
+      <div key={rows[index].rowId} style={{ display: 'flex' }}>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+        <div>{rows[index].rowId}</div>
+        <div>{rows[index].id}</div>
+      </div>
     </div>
+  );
+
+  return (
+    <List
+      width={1200}
+      height={700}
+      rowRenderer={renderRow}
+      rowCount={rows.length}
+      rowHeight={120}
+    />
   );
 }
 
